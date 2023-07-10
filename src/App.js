db02d4ca5fa9833dc57353162e00ed43840ca9ec
roc102 <kyroc102@gmail.com>
2023-07-10 16:00:23 +0530

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
-// import Game from './components/Game/Game';
+import Game from './components/Game/Game';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/play" element={<Game />} /> */}
+        <Route path="/play" element={<Game />} />
       </Routes>
     </Router>
   );
